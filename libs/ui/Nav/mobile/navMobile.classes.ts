47cc857d9e9fbcfd1e0bd918b6f383ffb583a0d4
masis_dk <masis.professionnel@gmail.com>
2025-06-08 16:44:46 +0200

    feat: section realisations

--- libs/ui/Nav/mobile/navMobile.classes.ts
+++ libs/ui/Nav/mobile/navMobile.classes.ts
@@ -4,16 +4,16 @@ export const navMobileClasses = {
   root: cva(['px-24 py-32 w-full transition-all duration-300 ease-in-out'], {
     variants: {
       index: {
-        true: 'fixed top-0 z-50',
+        true: 'fixed top-0 z-[2000]',
         false: 'sticky top-0'
       },
       open: {
-        true: 'bg-dark-primary h-full',
+        true: 'bg-dark-primary size-dvh',
         false: 'bg-transparent'
       }
     }
   }),
-  link: cva(['text-center text-text-primary text-2rem uppercase font-black transition-all delay-150 duration-450 ease-in-out'], {
+  link: cva(['text-center text-text-primary text-2rem uppercase font-black transition-all delay-150 duration-450 ease-in-out cursor-pointer'], {
     variants: {
       open: {
         true: 'opacity-100 translate-none',
@@ -24,7 +24,7 @@ export const navMobileClasses = {
 }
 
 export const navLinkClasses = {
-  root: cva(['w-full h-full pt-[100px] transition-all duration-450 ease-in-out'], {
+  root: cva(['w-full size-dvh pt-[80px] transition-all duration-450 ease-in-out'], {
     variants: {
       open: {
         true: 'translate-none',
---
+++ libs/ui/Sections/components/RealisationSection/realisation.section.classes.ts
@@ -0,0 +1,9 @@
+import {cva} from "class-variance-authority";
+
+export const realisationSectionClasses = {
+  root: cva(['py-[150px] px-24 md:ps-[138px] md:flex md:justify-between md:items-center md:gap-[60px] xl:gap-[150px]']),
+  wrapper: cva(['h-fit relative z-[500] bg-dark-primary md:ps-12']),
+  title: cva(['font-black leading-40 text-2xl mb-9']),
+  imageMobile: cva(['my-24 md:hidden']),
+  imageDesktop: cva(['hidden md:block basis-1/2 xl:basis-3/5 shrink-0'])
+}
