1fe9d5518b7086e6f19983ba7b60e63d2f625f33
masis_dk <masis.professionnel@gmail.com>
2025-07-11 09:24:05 +0200

    fix video

--- libs/ui/Video/video.classes.ts
+++ libs/ui/Video/video.classes.ts
@@ -1,5 +1,12 @@
 import {cva} from "class-variance-authority";
 
 export const videoClasses = {
-  root: cva(['aspect-video w-full h-[90vh] object-cover'])
+  root: cva(['aspect-video w-full h-[90vh] object-cover'], {
+    variants: {
+      autoHeight: {
+        true: '!h-auto',
+        false: '',
+      }
+    }
+  })
 }
