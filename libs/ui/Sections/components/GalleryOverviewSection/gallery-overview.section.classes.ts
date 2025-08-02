f99259f99275747d5e9367953950da930674bc5f
masis_dk <masis.professionnel@gmail.com>
2025-08-02 15:58:47 +0200

    tavu

--- libs/ui/Sections/components/GalleryOverviewSection/gallery-overview.section.classes.ts
+++ libs/ui/Sections/components/GalleryOverviewSection/gallery-overview.section.classes.ts
@@ -11,11 +11,22 @@ export const galleryOverviewSectionClasses = {
         '5X1': 'grid-cols-5',
         '1X1': 'grid-cols-1',
         '2X1': 'grid-cols-2',
-        '2X2': 'grid-cols-2 grid-rows-2',
+        '2X2': 'grid-cols-12 grid-row-4 md:grid-rows-[360px_280px] md:pl-[172px] md:pr-24 mb-160 gap-16',
       },
     },
     defaultVariants: {
       variation: 'default',
     },
   }),
+}
+
+export const galleryOverview2x2SectionClasses = {
+  root: cva([], {
+    variants: {
+      variation: {
+        '5cols': 'col-span-12 md:col-span-6 lg:col-span-5',
+        '7cols': 'col-span-12 md:col-span-6 lg:col-span-7'
+      }
+    }
+  })
 }
