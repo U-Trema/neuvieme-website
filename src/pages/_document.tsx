c8036fc844184c74acaac73c340da6b27c7afaa2
masis_dk <masis.professionnel@gmail.com>
2025-11-19 11:38:17 +0100

    fix: gsap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,31 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head/>
       <body className="antialiased">
-        <Main />
-        <NextScript />
-        <Script src="/js/gsap.js"  strategy='afterInteractive'/>
-        <Script src="/js/Draggable.js" strategy='afterInteractive' />
-        <Script src="/js/Flip.js" strategy='afterInteractive' />
-        <Script src="/js/SplitText.js" strategy='afterInteractive' />
-        <Script src="/js/imagesloaded.pkgd.min.js" strategy='beforeInteractive' />
+      <Main/>
+      <NextScript/>
+      <Script src="/js/gsap.js" strategy='afterInteractive'/>
+      <Script src="/js/Draggable.js" strategy='afterInteractive'/>
+      <Script src="/js/Flip.js" strategy='afterInteractive'/>
+      <Script src="/js/SplitText.js" strategy='afterInteractive'/>
+      <Script src="/js/imagesloaded.pkgd.min.js" strategy='afterInteractive'/>
+
+      <Script
+        id="register-gsap-plugins"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        function registerGsapPlugins() {
+          if (window.gsap && window.Draggable && window.Flip && window.SplitText) {
+            window.gsap.registerPlugin(window.Draggable, window.Flip, window.SplitText);
+          }
+        }
+        registerGsapPlugins();
+        window.addEventListener('load', registerGsapPlugins);
+      `,
+        }}
+      />
       </body>
     </Html>
   )
